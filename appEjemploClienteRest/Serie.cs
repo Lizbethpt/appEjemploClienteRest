72c094ef6057564b8ae144d3c8c1634b334656c7
lpena <lpena@LIZBETH>
2023-04-18 22:24:33 -0600

    Agregar archivos de proyecto.

---
+++ appEjemploClienteRest/Serie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace appEjemploClienteRest
+{
+    [DataContract]
+    internal class Serie
+    {
+        [DataMember(Name = "titulo")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "idSerie")]
+        public string IdSerie { get; set; }
+
+        [DataMember(Name = "datos")]
+        public DataSerie[] Data { get; set; }
+    }
+}
---
+++ appEjemploClienteRest/SeriesResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace appEjemploClienteRest
+{
+    [DataContract]
+    internal class SeriesResponse
+    {
+        [DataMember(Name = "series")]
+        public Serie[] series { get; set; }
+    }
+}
